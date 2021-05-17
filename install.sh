41708dfe8e29bc3fa730eca5df7e8a8006292a46
Joerg Simon <j_simon@mac.com>
2021-05-17 22:17:49 +0200

    add install.sh in case yml does not work

---
+++ install.sh
@@ -0,0 +1,10 @@
+conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorc
+pip install opencv-python
+conda install -c conda-forge ray-tune
+pip install ray
+conda install tabulate
+pip install -U tabulate
+pip install tensorboardX
+conda install matplotlib
+conda install scipy
+conda install tqdm
