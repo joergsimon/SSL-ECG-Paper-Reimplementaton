0b8cd066a12e4db0fff2422b57a1dc39fb9a7d68
Joerg Simon <j_simon@mac.com>
2021-05-23 22:54:22 +0200

    for debugging, remove the DataParallel for now

--- src/finetune_to_target.py
+++ src/finetune_to_target.py
@@ -130,8 +130,8 @@ def finetune_to_target_full_config(hyperparams_config, checkpoint_dir=None, targ
     if train_on_gpu:
         model = model.cuda()
         criterion = criterion.cuda()
-        if torch.cuda.device_count() > 1:
-            model = nn.DataParallel(model)
+        # if torch.cuda.device_count() > 1:
+        #     model = nn.DataParallel(model)
     finetune(model, optimizer, criterion, dataset, train_on_gpu, default_params, target_id)
 
 
