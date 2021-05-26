fbb19ac404ed29e32dcdc80f0449020ed60f936e
Joerg Simon <j_simon@mac.com>
2021-05-26 16:58:56 +0200

    training setting of 1080-ti

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ from src.constants import Constants as c
 import src.augmentations as aug
 
 run_tests = False
-run_pretrain_hyperparams = True
+run_pretrain_hyperparams = False
 pre_train_single_augmentation = False
 run_finetune_hyperparams = True
-pre_train_single = False
+pre_train_single = True
 fine_tune_single = False
 run_example = False
 
@@ -22,12 +22,13 @@ if __name__ == '__main__':
     if pre_train_single_augmentation:
         c.use_ray = False
         ptoc.train_pretext_full_config(pt.good_params_for_single_run, aug.AugmentationTypes.TIME_WRAP, use_tune=False)
+    if pre_train_single:
+        c.use_ray = False
+        pt.train_pretext_full_config(pt.good_params_for_single_run, use_tune=False)
     if run_finetune_hyperparams:
         c.use_ray = True
         ftt.train_finetune_tune_task(data.DataSets.AMIGOS, 'test_123', num_samples=20)
     c.use_ray = False
-    if pre_train_single:
-        pt.train_pretext_full_config(pt.good_params_for_single_run, use_tune=False)
     if fine_tune_single:
         ftt.finetune_to_target_full_config(ftt.good_params_for_single_run, checkpoint_dir=None, target_dataset=data.DataSets.AMIGOS, target_id='test_123')
     if run_example:
