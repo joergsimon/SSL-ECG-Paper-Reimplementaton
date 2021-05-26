1911b9d018c7610d2c0334fb1737292aa726a486
Joerg Simon <j_simon@mac.com>
2021-05-26 16:46:48 +0200

    just train pretext once and run hyper on tuning

--- main.py
+++ main.py
@@ -8,24 +8,28 @@ from src.constants import Constants as c
 import src.augmentations as aug
 
 run_tests = False
-run_hyperparam = True
+run_pretrain_hyperparams = False
+run_finetune_hyperparams = True
 pre_train_single_augmentation = False
 pre_train_single = False
 fine_tune_single = False
 run_example = False
 
 if __name__ == '__main__':
-    if run_hyperparam:
+    if run_pretrain_hyperparams:
+        c.use_ray = True
         pt.train_pretext_tune_task(num_samples=6)
-        ftt.train_finetune_tune_task(data.DataSets.AMIGOS, 'test_123', num_samples=20)
-    else:
-        c.use_ray = False
     if pre_train_single_augmentation:
+        c.use_ray = False
         ptoc.train_pretext_full_config(pt.good_params_for_single_run, aug.AugmentationTypes.TIME_WRAP, use_tune=False)
+    if run_finetune_hyperparams:
+        c.use_ray = True
+        ftt.train_finetune_tune_task(data.DataSets.AMIGOS, 'test_123', num_samples=20)
+    c.use_ray = False
     if pre_train_single:
         pt.train_pretext_full_config(pt.good_params_for_single_run, use_tune=False)
     if fine_tune_single:
-        ftt.finetune_to_target_full_config(ftt.good_params_for_single_run, checkpoint_dir=None, target_dataset=data.DataSets.AMIGOS, target_id='test_123', )
+        ftt.finetune_to_target_full_config(ftt.good_params_for_single_run, checkpoint_dir=None, target_dataset=data.DataSets.AMIGOS, target_id='test_123')
     if run_example:
         re.run_example(data.DataSets.AMIGOS, 'test_123')
     if run_tests:
