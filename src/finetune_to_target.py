fixing a servious issue with gradients compution
they where also computed on evaluation iteration

src/finetune_to_target.py
@@ -194,7 +194,7 @@ def finetune_to_target_full_config(hyperparams_config, checkpoint_dir=None, targ
         training = default_params.epochs - warmup
         cycles = scheduler_info['cycles']
         schedulder = get_cosine_with_hard_restarts_schedule_with_warmup(optimizer, warmup, training, cycles)
-        
+
     criterion = nn.BCEWithLogitsLoss()#nn.CrossEntropyLoss()
 
     # The `checkpoint_dir` parameter gets passed by Ray Tune when a checkpoint

src/pretext_training.py
@@ -187,9 +187,9 @@ def train_pretext(model, optimizer, schedulder, criterion, train_on_gpu: bool, p
                     predicted = torch.argmax(tasks_out, dim=1).detach()
                     accuracy = torch.sum(predicted == aug_labels).type(torch.float) / aug_labels.shape[0]
 
-                    task_loss.backward()
-                    optimizer.step()
-                    # schedulder.step()
+                    if loss_type == 'train':
+                        task_loss.backward()
+                        optimizer.step()
 
                     total_loss = utils.assign(total_loss, task_loss)
                     total_accuracy = utils.assign(total_accuracy, accuracy)
@@ -220,7 +220,8 @@ def train_pretext(model, optimizer, schedulder, criterion, train_on_gpu: bool, p
         # validate the model #
         ######################
         model.eval()
-        iterate_batches(valid_loader, 'valid')
+        with torch.no_grad():
+            iterate_batches(valid_loader, 'valid')
 
         if schedulder is not None:
             schedulder.step()

src/training/training_helper.py
@@ -3,35 +3,42 @@ import src.utils as utils
 import torch
 from ray import tune
 import os.path
+from enum import Enum
+
+class TrainingMode(Enum):
+    TRAIN = 0
+    VALID = 2
 
 
-def iterate_batches(loader, optimizer, scaler, batch_size, train_on_gpu: bool, compute_loss):
+def iterate_batches(loader, optimizer, scaler, batch_size, train_on_gpu: bool, compute_loss, mode: TrainingMode):
     total_loss = 0.0
     total_accuracy = 0.0
     total_accuracy_list = []
+
     for i_batch, (data, labels) in enumerate(utils.pbar(loader, leave=False)):
         # labels = torch.stack(labels, dim=1).float()
         if data.shape[0] != batch_size:
-            #print('skipping too small batch')
+            # print('skipping too small batch')
             continue  # if not full batch, just continue
         if train_on_gpu:
             data, labels = data.cuda(), labels.cuda()
         optimizer.zero_grad()
         loss, accuracy = compute_loss(data, labels)
-        if scaler is not None:
-            scaler.scale(loss).backward()
-            scaler.step(optimizer)
-            scaler.update()
-        else:
-            loss.backward()
-            optimizer.step()
+        if mode == TrainingMode.TRAIN:
+            if scaler is not None:
+                scaler.scale(loss).backward()
+                scaler.step(optimizer)
+                scaler.update()
+            else:
+                loss.backward()
+                optimizer.step()
         total_accuracy_list.append(accuracy.item())
         total_loss += loss.item()
         total_accuracy += accuracy.item()
-    # print(f'all accuracies: {total_accuracy_list}')
-    # print(f'list based accuracy: {np.mean(np.array(total_accuracy_list))}')
-    # print(f'sum of accuracy: {total_accuracy.item()}')
-    # print(f'sum of accuracy normalised by loader length: {total_accuracy.item() / len(loader)}')
+        # print(f'all accuracies: {total_accuracy_list}')
+        # print(f'list based accuracy: {np.mean(np.array(total_accuracy_list))}')
+        # print(f'sum of accuracy: {total_accuracy.item()}')
+        # print(f'sum of accuracy normalised by loader length: {total_accuracy.item() / len(loader)}')
     return total_loss, total_accuracy
 
 
@@ -57,7 +64,7 @@ def std_train_loop(epochs, batch_size, train_loader, valid_loader, model, optimi
         # train the model #
         ###################
         model.train()
-        l, a = iterate_batches(train_loader, optimizer, scaler, batch_size, train_on_gpu, compute_loss_and_accuracy)
+        l, a = iterate_batches(train_loader, optimizer, scaler, batch_size, train_on_gpu, compute_loss_and_accuracy, TrainingMode.TRAIN)
         train_loss += l
         train_accuracy += a
 
@@ -65,7 +72,8 @@ def std_train_loop(epochs, batch_size, train_loader, valid_loader, model, optimi
         # validate the model #
         ######################
         model.eval()
-        l, a = iterate_batches(valid_loader, optimizer, scaler, batch_size, train_on_gpu, compute_loss_and_accuracy)
+        with torch.no_grad():
+            l, a = iterate_batches(valid_loader, optimizer, scaler, batch_size, train_on_gpu, compute_loss_and_accuracy, TrainingMode.VALID)
         valid_loss += l
         valid_accuracy += a
 